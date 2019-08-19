4cc3c92c3df44e92e3b4558959ab3d09c7594d77
Natalie Taktachev <ntaktach@gmail.com>
2019-08-19 23:05:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,4 +20,23 @@ class Student
     DB[:conn].execute(sqlite)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  end
+
+
 end
